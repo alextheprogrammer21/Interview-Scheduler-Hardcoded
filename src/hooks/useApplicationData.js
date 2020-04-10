fd7db60e4a0e879718bfe11417a3bb8a84e743d0
Alex <alex@Alexs-MacBook-Air.local>
2020-04-09 19:20:02 -0700

    Added proxy and removed hardcoding of data from the axios requests

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -24,9 +24,9 @@ export default function useApplicationData() {
 
   React.useEffect(() => {
     Promise.all([
-      Promise.resolve(axios.get('http://localhost:8001/api/days')),        
-      Promise.resolve(axios.get('http://localhost:8001/api/appointments')),
-      Promise.resolve(axios.get('http://localhost:8001/api/interviewers')),
+      Promise.resolve(axios.get('/api/days')),        
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers')),
     ]).then((all) => {
       setState(prev => ({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
@@ -43,7 +43,7 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
+    return axios.put(`/api/appointments/${id}`, appointment)
     .then(() => {
       state.days[calenderDay].spots -= 1;
       setState({
@@ -59,7 +59,7 @@ export default function useApplicationData() {
       ...state.appointments[id],
       interview: null
     };
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`, appointment)
+    return axios.delete(`/api/appointments/${id}`, appointment)
     .then(() => {
       state.days[calenderDay].spots += 1;
     })
